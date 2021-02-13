8a033593d839f217ef2699ee95b8ef9dfc014fb1
Ivelin Penchev <ivelinpenchev@gmail.com>
2021-02-13 14:24:16 +0000

    Initial auth0 integration

--- js/website/src/App.tsx
+++ js/website/src/App.tsx
@@ -1,7 +1,17 @@
 import Router from "Router";
+import { Auth0Provider } from "@auth0/auth0-react";
+import config from "config";
 
 const App = () => {
-  return <Router />;
+  return (
+    <Auth0Provider
+      domain={config.AUTH0_DOMAIN}
+      clientId={config.AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <Router />
+    </Auth0Provider>
+  );
 };
 
 export default App;
