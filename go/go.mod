7be6184a0f115f7e8054d3527a781874509814a0
Ivelin Penchev <ivelinpenchev@gmail.com>
2021-02-13 16:15:46 +0000

    Added simple API

---
+++ go/go.mod
@@ -0,0 +1,5 @@
+module github.com/iv-p/react-go-saas-starter
+
+go 1.15
+
+require github.com/kataras/iris/v12 v12.2.0-alpha2.0.20210212151223-5fe233278a28 // indirect
---
+++ go/svc/api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+    app := iris.New()
+    app.Get("/", index)
+    app.Listen(":8080")
+}
+
+func index(ctx iris.Context) {
+    ctx.Writef("ok")
+}
